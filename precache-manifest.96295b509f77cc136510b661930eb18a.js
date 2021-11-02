27096566a290ab6163c24edf303b8563a9fc487f
Leandro <play3rtox1c@gmail.com>
2021-11-02 11:02:25 -0300

    Updates

---
+++ precache-manifest.96295b509f77cc136510b661930eb18a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69ee8523ed4ca651fc03187ecf7889ee",
+    "url": "/mood-store2.0/index.html"
+  },
+  {
+    "revision": "309c514eac77983827b2",
+    "url": "/mood-store2.0/static/css/main.31cc85fe.chunk.css"
+  },
+  {
+    "revision": "016929b8887669c30137",
+    "url": "/mood-store2.0/static/js/2.d5583729.chunk.js"
+  },
+  {
+    "revision": "2d0dc3ea56ea29aa89323d664321ba6d",
+    "url": "/mood-store2.0/static/js/2.d5583729.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "309c514eac77983827b2",
+    "url": "/mood-store2.0/static/js/main.ad5b15d1.chunk.js"
+  },
+  {
+    "revision": "a62fb16c58e5a329fd04",
+    "url": "/mood-store2.0/static/js/runtime-main.17834ea6.js"
+  },
+  {
+    "revision": "5b5a277cd763d5367dcf4306903edd85",
+    "url": "/mood-store2.0/static/media/loading.5b5a277c.gif"
+  }
+]);
